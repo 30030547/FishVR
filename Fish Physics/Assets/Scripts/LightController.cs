46f95abcf0302d66db71042e07a211079269849a
30030547 <30030547@students.southwales.ac.uk>
2022-11-04 20:34:07 +0000

    Basic 2D Fish Game
    Created an environment for the fish to move in using collisions for the tank. Player / Water movement physics implemented. Lighting for the scene to add atmosphere was also added using Pixel Perfect Lights and Particles.

---
+++ Fish Physics/Assets/Scripts/LightController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DG.Tweening;
+
+public class LightController : MonoBehaviour
+{
+    public UnityEngine.Rendering.Universal.Light2D explosionLight;
+    public float explosionLightIntensity;
+
+    void Start()
+    {
+        DOVirtual.Float(0, explosionLightIntensity, .05f, ChangeLight).OnComplete(() => DOVirtual.Float(explosionLightIntensity, 0, .1f, ChangeLight));
+    }
+
+    void ChangeLight(float x)
+    {
+        explosionLight.intensity = x;
+    }
+}
---
+++ Fish Physics/Assets/Scripts/Movement.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+
+    ParticleSystem gun;
+    Animator anim;
+    private float moveSpeed;
+    private float jumpForce;
+    private bool isJumping;
+    private float x;
+    private float y;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+        gun = GetComponentInChildren<ParticleSystem>();
+        moveSpeed = 3f;
+        jumpForce = 10f;
+        isJumping = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+         x = Input.GetAxisRaw("Horizontal");
+         y = Input.GetAxisRaw("Vertical");
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gun.Play();
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            gun.Stop();
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        if (x > 0.1f || x < -0.1f)
+        {
+            rb2D.AddForce(new Vector2(x * moveSpeed, 0f), ForceMode2D.Impulse);
+        }
+
+        if (!isJumping && y > 0.1f)
+        {
+            rb2D.AddForce(new Vector2(0f, y * jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            isJumping = false;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            isJumping = true;
+        }
+    }
+
+}
